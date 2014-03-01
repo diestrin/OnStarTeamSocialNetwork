7748dde2e9e4529c3644864751781b51b498cf73
Diego Barahona <diestrin@gmail.com>
2014-02-28 22:31:15 -0600

    Bootstraping the project with yeoman
    Generating the project with the generator-angular
    for yeoman, just run npm install and bower install

---
+++ app/scripts/app.js
@@ -0,0 +1,16 @@
+'use strict';
+
+angular.module('ostApp', [
+  'ngSanitize',
+  'ngRoute'
+])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
